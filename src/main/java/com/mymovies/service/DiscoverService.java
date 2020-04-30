b1c41bb31c45b7bb4ab8da5f9d8bd5978b19a2dd
Ranushan Rachu <ranusha.n@hotmail.fr>
2020-04-30 15:55:23 +0200

    [ORIGIN][MASTER][UPDATED] centralize configuration

--- src/main/java/com/mymovies/service/DiscoverService.java
+++ src/main/java/com/mymovies/service/DiscoverService.java
@@ -4,23 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RefreshScope
 public class DiscoverService implements IDiscoverService {
 
-	@Value("${resource.api.url.base}")
-	private String BASE_URL;
-	
-	@Value("${resource.api.url.image}")
-	private String BASE_URL_IMAGE;
-	
-	@Value("${resource.api.key}")
-	private String API_KEY;
-	
-	@Value("${resource.api.language}")
-	private String Language;
+	@Value("${service.url}")
+	private String serviceUrl;
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(DiscoverService.class);
 	
@@ -31,7 +24,7 @@ public class DiscoverService implements IDiscoverService {
 
 		String discover = null;
 
-		String url = BASE_URL+API_KEY+Language+"&sort_by=popularity.desc&include_adult=false&include_video=false&year=2019&page=";
+		String url = serviceUrl;
 				
 		try {
 			discover = restTemplate.getForObject(url+page_number, String.class);
